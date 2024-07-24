b72d644ba696883e59abde8048bf74e663b70bb2
Filipe Monteiro <filipebsmonteiro@gmail.com>
2024-07-24 13:43:44 -0300

    Backend Finished

--- server/transcriber.js
+++ server/transcriber.js
@@ -1,34 +1,57 @@
 import EventEmitter from "events";
+import { createClient, LiveConnectionState, LiveTranscriptionEvents } from "@deepgram/sdk";
+
+const DEEPGRAM_API_KEY = `54dddd62a18582d9e22f1338bed3019094a1d562`;
 
 class Transcriber extends EventEmitter {
   constructor() {
     super();
   }
+  connection = null
 
   // sampleRate: number
-  startTranscriptionStream(sampleRate) {
+  async startTranscriptionStream(sampleRate) {
     // example deepgram configuration
-    /*
-    {
-      model: "nova-2",
-      punctuate: true,
-      language: "en",
-      interim_results: true,
-      diarize: false,
-      smart_format: true,
-      endpointing: 0,
-      encoding: "linear16",
-      sample_rate: sampleRate,
+    // const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+    try {
+      const deepgram = createClient(DEEPGRAM_API_KEY);
+
+      const connection = deepgram.listen.live({
+        model: "nova-2",
+        punctuate: true,
+        language: "en-US",
+        smart_format: true,
+        interim_results: true,
+        // diarize: false,
+        // endpointing: 0,
+        encoding: "linear16",
+        sample_rate: sampleRate,
+      });
+      this.connection = connection;
+      return connection
+    } catch (error) {
+      console.error(`Failed to start Transcription Stream`, error.message)
+      throw new Error(`Failed to start Transcription Stream`)
     }
-      */
   }
 
   endTranscriptionStream() {
-    // close deepgram connection here
+    try {
+      this.connection.removeAllListeners();
+      this.connection.finish();
+    } catch (error) {
+      console.error(`Error on end Transcription Stream`, error.message)
+    }
   }
 
   // NOTE: deepgram must be ready before sending audio payload or it will close the connection
-  send(payload) {}
+  send(payload) {
+    if (this.connection && this.connection.getReadyState() !== LiveConnectionState.OPEN) {
+      console.warn(`Connection is ${LiveConnectionState[this.connection.getReadyState()]}. Unable to Send Payload.`)
+      return;
+    }
+    this.connection.send(payload)
+  }
 
   // ... feel free to add more functions
 }
--- server/websocket.js
+++ server/websocket.js
@@ -1,3 +1,4 @@
+import { LiveTranscriptionEvents } from "@deepgram/sdk";
 import Transcriber from "./transcriber.js";
 
 /**
@@ -18,9 +19,101 @@ import Transcriber from "./transcriber.js";
 
 const initializeWebSocket = (io) => {
   io.on("connection", (socket) => {
-    console.log(`connection made (${socket.id})`);
-
     // ... add needed event handlers and logic
+    console.log(`connection made (${socket.id})`); 
+    const transcriber = new Transcriber()
+    // let connection = new Transcriber()
+    let sampleRate = null;
+
+    const startTranscriptionStream = async (config) => {
+      if (transcriber.connection) {
+        return
+      }
+
+      if (typeof config !== 'object' || !config.sampleRate) {
+        console.warn(`No Config Provided`)
+      }
+      sampleRate = config?.sampleRate ? config.sampleRate : sampleRate
+
+      if (!sampleRate) {
+        throw new Error(`Invalid Configuration`)
+      }
+
+      await transcriber.startTranscriptionStream(sampleRate)
+    }
+
+    const addListeners = () => {
+      const connection = transcriber.connection
+      
+      connection.on(LiveTranscriptionEvents.Close, () => {
+        console.log("Transcription Connection closed.");
+      });
+
+      connection.on(LiveTranscriptionEvents.Transcript, (data) => {
+        if (data.channel.alternatives.length) {
+          const { transcript } = data.channel.alternatives[0];
+          if (data.speech_final) {
+            socket.emit('final', transcript)
+          } else {
+            socket.emit('partial', transcript)
+          }
+        }
+      });
+
+      connection.on(LiveTranscriptionEvents.Open, () => {
+        console.log("Transcription Connection Open.");
+        // console.log('socket :>> ', socket.emit('transcriber-ready'));
+        socket.emit("transcriber-ready");
+      })
+
+      connection.on(LiveTranscriptionEvents.Error, (error) => {
+        console.log("Transcription Error.");
+        socket.emit('error', error.message)
+      });
+    }
+
+    socket.on('configure-stream', async (config) => {
+      try {
+        await startTranscriptionStream(config)
+        addListeners()
+      } catch (error) {
+        console.warn('Error on Configure Stream')
+        console.warn(error.message)
+        socket.emit(`error`, `Error on Configure Stream`)
+      }
+    })
+
+    socket.on("incoming-audio", async (audioPm16) => {
+      try {
+        if (!ArrayBuffer.isView(audioPm16) && !(audioPm16 instanceof Buffer)) {
+          throw new Error(`Invalid audio Data`)
+        }
+  
+        if (!transcriber.connection) {
+          await startTranscriptionStream()
+          transcriber.send(audioPm16)
+          return
+        }
+  
+        transcriber.send(audioPm16)
+      } catch (error) {
+        console.warn(`Error sending audio data: `, error.message)
+        socket.emit("error", "Failed to Process audio data")
+      }
+    })
+
+    socket.on('stop-stream', () => {
+      transcriber.endTranscriptionStream();
+    })
+
+    socket.on('disconnect', () => {
+      console.log(`Socket (${socket.id}) disconnected`); 
+      transcriber.endTranscriptionStream();
+    })
+
+    
+    // socket.emit('final', data)
+    // socket.emit('partial', data)
   });
 
   return io;
